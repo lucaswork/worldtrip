bde969fd00fb5042b9312b63efbb592825317e0a
lucaswork <lucasdmwork@gmail.com>
2023-05-11 19:18:35 -0300

    settings up swiper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import Head from "next/head";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+
+
 export default function Home() {
-    return (
+  return (
+    <>
       <h1>Dashboard</h1>
-    )
+
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
+    </>
+  );
 }
